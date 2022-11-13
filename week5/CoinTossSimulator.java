076b44444f0345399fe0ccb5bdf4653b18e4dc85
Nick-Stoyanov <99138648+Nick-Stoyanov@users.noreply.github.com>
2022-11-13 08:44:02 -0500

    Add files via upload

--- week5/CoinTossSimulator.java
+++ week5/CoinTossSimulator.java
@@ -1,13 +1,92 @@
 /**
- * 
+ * Program to simulate the tossing of a coin
  */
 package chapter6;
 
+import javax.swing.JOptionPane;
+
 /**
- * @author Nikolay Stoyanov
- * Nov 8, 2022
+ * 6.16 Coin Toss Simulator
+ * 
+ * Write a class named Coin. The Coin class should have the following field: A
+ * String named sideUp. The sideUp field will hold either “heads” or “tails”
+ * indicating the side of the coin that is facing up. The Coin class should have
+ * the following methods: A no-arg constructor that randomly determines the side
+ * of the coin that is facing up (“heads” or “tails”) and initializes the sideUp
+ * field accordingly. A void method named toss that simulates the tossing of the
+ * coin. When the toss method is called, it randomly determines the side of the
+ * coin that is facing up (“heads” or “tails”) and sets the sideUp field
+ * accordingly. A method named getSideUp that returns the value of the sideUp
+ * field. Write a program that demonstrates the Coin class. The program should
+ * create an instance of the class and display the side that is initially facing
+ * up. Then, use a loop to toss the coin 20 times. Each time the coin is tossed,
+ * display the side that is facing up. The program should keep count of the
+ * number of times heads is facing up and the number of times tails is facing
+ * up, and display those values after the loop finishes.
+ * 
+ * @author Nikolay Stoyanov Nov 12, 2022
  */
 public class CoinTossSimulator
 {
+	/**
+	 * Default constructor
+	 */
+	public CoinTossSimulator()
+	{
+		super();
+	}
 
-}
+	/**
+	 * Main method tosses the coin 20 times and keeps count of heads and tails
+	 * 
+	 * @param args ignored
+	 */
+
+	private static void playGame()
+	{
+		/**
+		 * Constant for how many times coin should be tossed
+		 */
+		final Integer rolls = 20;
+
+		// initialize heads counter
+		Integer headsCount = 0;
+
+		// Initialize tails counter
+		Integer tailsCount = 0;
+
+		// Create new coin
+		Coin coin = new Coin();
+		System.out.println("Initial state: " + coin.getSideUp());
+
+		// loop to iterate and keep count of heads and tails
+		for (int i = 0; i < rolls; i++)
+		{
+			coin.toss();
+			System.out.println(coin.getSideUp());
+
+			if (coin.getSideUp() == "heads")
+			{
+				headsCount++;
+			} else
+			{
+				tailsCount++;
+			}
+		}
+		JOptionPane.showMessageDialog(null,"Heads count: " + headsCount + "\nTails count: " + tailsCount);
+
+	}
+
+	/**
+	 * Main method calls the playGame method to start game
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args)
+	{
+		playGame();
+	
+	}// close main
+
+}// close class
--- week5/TossingCoinsForADollar.java
+++ week5/TossingCoinsForADollar.java
@@ -1,13 +1,114 @@
 /**
- * 
+ * Program to simulate a Tossing for Dollar Game
  */
 package chapter6;
 
+import javax.swing.JOptionPane;
+
 /**
- * @author Nikolay Stoyanov
- * Nov 8, 2022
+ * 6.18 Tossing Coins for a Dollar
+ * 
+ * For this assignment you will create a game program using the Coin class from
+ * Programming Challenge 17. The program should have three instances of the Coin
+ * class: one representing a quarter, one representing a dime, and one
+ * representing a nickel. When the game begins, your starting balance is $0.
+ * During each round of the game, the program will toss the simulated coins.
+ * When a coin is tossed, the value of the coin is added to your balance if it
+ * lands heads-up. For example, if the quarter lands heads-up, 25 cents is added
+ * to your balance. Nothing is added to your balance for coins that land
+ * tails-up. The game is over when your balance reaches one dollar or more. If
+ * your balance is exactly one dollar, you win the game. You lose if your
+ * balance exceeds one dollar.
+ * 
+ * @author Nikolay Stoyanov Nov 12, 2022
  */
 public class TossingCoinsForADollar
 {
 
-}
+	/**
+	 * Default constructor
+	 */
+	public TossingCoinsForADollar()
+	{
+		super();
+	}
+	
+	
+	private static void playGame() {
+		/**
+		 * Initialize constant value for quarter
+		 */
+		final double quarterValue = 0.25;
+		/**
+		 * Initialize constant value for dime
+		 */
+		final double dimeValue = 0.1;
+		/**
+		 * Initialize constant value for nickel
+		 */
+		final double nickelValue = 0.05;
+
+		// Initialize value to keep total
+		Double totalMoney = 0.0;
+
+		// Create three new coins
+		Coin quarter = new Coin();
+		Coin dime = new Coin();
+		Coin nickel = new Coin();
+
+		// while loop keeps tossing coin until user reaches or exceeds 1 dollar
+		while (totalMoney < 1)
+		{
+			quarter.toss();
+
+			if (quarter.getSideUp() == "heads" && totalMoney < 1)
+			{
+				totalMoney += quarterValue;
+				System.out.println("Quarter landed heads up.\n$0.25 added to your total.");
+			} else
+			{
+				System.out.println("Quarter landed tails up.\n0 added to your total.");
+			}
+			dime.toss();
+			if (dime.getSideUp() == "heads" && totalMoney < 1)
+			{
+				totalMoney += dimeValue;
+				System.out.println("Dime landed heads up.\n$0.10 added to your total.");
+			} else
+			{
+				System.out.println("Dime landed tails up.\n0 added to your total.");
+			}
+			nickel.toss();
+			if (nickel.getSideUp() == "heads" && totalMoney < 1)
+			{
+				totalMoney += nickelValue;
+				System.out.println("Nickel landed heads up.\n$0.05 added to your total.");
+			} else
+			{
+				System.out.println("Nickel landed tails up.\n0 added to your total.");
+			}
+			System.out.println("Total:" + totalMoney);
+		}
+
+		// Determine if user wins ($1) or loses (over $1)
+		if (totalMoney == 1)
+		{
+			JOptionPane.showMessageDialog(null, "Your got exactly $1! You win");
+		} else
+		{
+			JOptionPane.showMessageDialog(null, "You got: $" + totalMoney + "\nYou lose!");
+		}
+	}
+
+	/**
+	 * main method calls playGame method to initiate game
+	 * 
+	 * @param args ignored
+	 */
+	public static void main(String[] args)
+	{
+		playGame();
+		
+	}
+
+}// close class
